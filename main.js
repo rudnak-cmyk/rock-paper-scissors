b028ad6a74148baf0641ba536d2b6a70fe2a2b3e
rudnak-cmyk <rmokhnenko23@gmail.com>
2024-02-20 14:50:53 +0300

    rock paper scissors logic js

---
+++ main.js
@@ -0,0 +1,25 @@
+function getPlayerChoise(){
+    let result = prompt("Choose Rock, Paper or Scissors: ");
+    return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
+function getComputerChoise(){
+    let result = Math.floor(Math.random * 3);
+}
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection)
+        return "Draw";
+    else if (playerSelection == 'Rock' && computerSelection == 'Paper')
+        return "You Lose";
+    else if (playerSelection == 'Rock' && computerSelection == 'Scissors')
+        return "You Win";
+    else if (playerSelection == 'Paper' && computerSelection == 'Rock')
+        return "You Win";
+    else if (playerSelection == 'Paper' && computerSelection == 'Scissors')
+        return "You Lose";
+    else if (playerSelection == 'Scissors' && computerSelection == 'Rock')
+        return "You Lose";
+    else if (playerSelection == 'Scissors' && computerSelection == 'Paper')
+        return "You Win";
+}
