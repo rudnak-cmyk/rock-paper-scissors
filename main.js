b2f9741e84e2195993cdd5654af2447a12f3b3c5
rudnak-cmyk <rmokhnenko23@gmail.com>
2024-02-21 22:33:43 +0300

    at least working

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
+const body = document.querySelector('body');
+let score = 0;
+let cscore = 0;
+const scoreDiv = document.createElement('div');
+scoreDiv.textContent = `score: ${score}`;
+body.appendChild(scoreDiv);
+
 function getPlayerChoise(){
-    let result = prompt("Choose Rock, Paper or Scissors: ");
-    return result.charAt(0).toUpperCase() + result.slice(1);
+    return new Promise((resolve, reject) => {
+        const choiceContainer = document.createElement('div');
+        const rockButton = document.createElement('button');
+        rockButton.textContent = 'Rock 🗻';
+        const paperButton = document.createElement('button');
+        paperButton.textContent ='Paper 📃';
+        const scissorsButton = document.createElement('button');
+        scissorsButton.textContent = 'Scissors ✂️';
+
+        choiceContainer.appendChild(rockButton);
+        choiceContainer.appendChild(paperButton);
+        choiceContainer.appendChild(scissorsButton);
+
+        body.appendChild(choiceContainer);
+
+        rockButton.addEventListener('click', () => resolve('Rock'));
+        paperButton.addEventListener('click', () => resolve('Paper'));
+        scissorsButton.addEventListener('click', () => resolve('Scissors'));
+    });
 }
 
 function getComputerChoise(){
@@ -16,16 +40,55 @@ function getComputerChoise(){
 function playRound(playerSelection, computerSelection){
     if (playerSelection == computerSelection)
         return "Draw";
-    else if (playerSelection == 'Rock' && computerSelection == 'Paper')
-        return "You Lose";
-    else if (playerSelection == 'Rock' && computerSelection == 'Scissors')
-        return "You Win";
-    else if (playerSelection == 'Paper' && computerSelection == 'Rock')
-        return "You Win";
-    else if (playerSelection == 'Paper' && computerSelection == 'Scissors')
-        return "You Lose";
-    else if (playerSelection == 'Scissors' && computerSelection == 'Rock')
-        return "You Lose";
-    else if (playerSelection == 'Scissors' && computerSelection == 'Paper')
-        return "You Win";
+    else if (playerSelection == 'Rock' && computerSelection == 'Paper'){
+        ++cscore;
+        return "You Lose! Paper beats Rock";
+    }
+    else if (playerSelection == 'Rock' && computerSelection == 'Scissors'){
+        ++score;
+        return "You Win! Rock beats Scissors";
+    }
+    else if (playerSelection == 'Paper' && computerSelection == 'Rock'){
+        ++score;
+        return "You Win! Paper beats Rock";
+    }
+    else if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
+        ++cscore;
+        return "You Lose! Scissors beats Paper";
+    }
+    else if (playerSelection == 'Scissors' && computerSelection == 'Rock'){
+        ++cscore;
+        return "You Lose! Rock beats Scissors";
+    }
+    else if (playerSelection == 'Scissors' && computerSelection == 'Paper'){
+        ++score;
+        return "You Win! Scissors beats Paper";
+    }
+}
+
+const play = document.querySelector('#playButton');
+play.addEventListener('click', async function(){
+    body.removeChild(play);
+    const playerChoice = await getPlayerChoise();
+    const computerChoice = getComputerChoise();
+    alert(playRound(playerChoice, computerChoice));
+    body.removeChild(choiceContainer);
+});
+
+if (score === 5){
+    if (score > cscore){
+        score = 0;
+        cscore = 0;
+        alert('You Win');
+    }
+    else if (score < cscore){
+        score = 0;
+        cscore = 0;
+        alert ('You Lose');
+    }
+    else{
+        score = 0;
+        cscore = 0;
+        alert('Draw');
+    }
 }
